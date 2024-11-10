1adb5a22c5643e216c88c5e5fac87c30f87fc9a7
Albatol-Alrefaei <batoolah13@gmail.com>
2024-11-10 23:26:23 +0100

    changed material design and styles

--- app/src/main/java/com/example/firstassingment/MainActivity.kt
+++ app/src/main/java/com/example/firstassingment/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.firstassingment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_page.xml)
+        setContentView(R.layout.login_page)
+
+        findViewById<TextView>(R.id.register)?.setOnClickListener {
+            setContentView(R.layout.create_account)
+
+            findViewById<TextView>(R.id.login)?.setOnClickListener {
+                setContentView(R.layout.login_page)
+            }
+        }
     }
 }
